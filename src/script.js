4be1221e5bb234ff92abf87623178101eec33e06
Michelle <mwingter@gmail.com>
2020-08-28 23:49:54 -0300

    Links adicionados nos cargos

--- src/script.js
+++ src/script.js
@@ -17,24 +17,34 @@ fetch('https://www.mocky.io/v2/5d6fb6b1310000f89166087b')
     })
 
     var filtered = dataList.filter(isValid); // Utilizando método filter do javascript para filtrar os dados da API
-    //console.log("Dados filtrados: ", filtered)
-    var div_vagas = document.getElementById("vagas-api")
-    var div_local = document.getElementById("local-api")
+    console.log("Dados filtrados: ", filtered);
+    var div_vagas = document.getElementById("vagas-api");
+    var div_local = document.getElementById("local-api");
     
     filtered.forEach(function(vaga){
-        var text = document.createElement("p");
+        var list = document.createElement("ul");
+        var list_item = document.createElement("li");
+        var text = document.createElement("a");
         var local = document.createElement("p");
 
+        list.style.listStyle = "none"
+        list.style.padding = 0
+
         text.innerHTML = vaga.cargo;
+        text.href = vaga.link;
+        text.style.color = "inherit";
+
         if(vaga.localizacao){
-            local.innerHTML = vaga.localizacao.bairro + "-" + vaga.localizacao.cidade + "," + vaga.localizacao.pais
+            local.innerHTML = vaga.localizacao.bairro + " - " + vaga.localizacao.cidade + ", " + vaga.localizacao.pais;
         }
         else{
             local.innerHTML = "Remoto"
         }
-        
-        div_vagas.appendChild(text)
-        div_local.appendChild(local)
+
+        list.appendChild(list_item);
+        list_item.appendChild(text);
+        div_vagas.appendChild(list);
+        div_local.appendChild(local);
     })
 })
 
