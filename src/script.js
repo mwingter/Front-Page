58e1629de1a8cc59d1f72c732fb6488dd72c16f1
Michelle <mwingter@gmail.com>
2020-08-28 22:34:11 -0300

    github pages ainda não carregou os dados da api

--- src/script.js
+++ src/script.js
@@ -17,20 +17,20 @@ fetch('http://www.mocky.io/v2/5d6fb6b1310000f89166087b')
     })
 })
 
-// fazendo isso para carregar o array por completo, senão o javascript 
-// executa a próxima função considerando que ele está vazio
-setTimeout(function(){
-        console.log("dados recarregados", dataList);
-},1500)
+
+/*setTimeout(function(){
+        //console.log("dados recarregados", dataList);
+},1500)*/
 
 // Utilizando método filter do javascript para filtrar os dados da API
 function isValid(value) {
     return value.ativa;
 }
-
+// usando o timeout para carregar o array por completo, senão o javascript 
+// executa a próxima função considerando que ele está vazio
 setTimeout(function(){
         var filtered = dataList.filter(isValid);
-        console.log("Dados filtrados: ", filtered)
+        //console.log("Dados filtrados: ", filtered)
         var div_vagas = document.getElementById("vagas-api")
         var div_local = document.getElementById("local-api")
         
